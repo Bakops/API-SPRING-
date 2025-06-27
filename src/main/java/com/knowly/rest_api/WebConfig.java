68381abdb0e43b3d7a99f959bdeb1f6067405954
Bakops <bakoutoure4@gmail.com>
2025-06-27 11:33:20 +0200

    first commit

--- src/main/java/com/knowly/rest_api/WebConfig.java
+++ src/main/java/com/knowly/rest_api/WebConfig.java
@@ -4,19 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // Allow CORS for all paths (or specify specific paths if needed)
-        registry.addMapping("/**")
-                .allowedOrigins("https://knowly-app-front-next-88aa-omega.vercel.app")  // Your frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
-                .allowedHeaders("*")  // Allow all headers (or specify specific headers)
-                .allowCredentials(true);  // Allow credentials like cookies or authorization headers
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://knowly-app-front-next.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
