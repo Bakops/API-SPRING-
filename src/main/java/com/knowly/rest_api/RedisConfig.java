ba75d4ca1e52ba9505a3ebda77625812d520be3a
Bakops <bakoutoure4@gmail.com>
2025-06-28 16:26:44 +0200

    MAJ RedisConfig.java

--- src/main/java/com/knowly/rest_api/RedisConfig.java
+++ src/main/java/com/knowly/rest_api/RedisConfig.java
@@ -2,19 +2,49 @@ package com.knowly.rest_api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
+import jakarta.annotation.PreDestroy;
+
 @Configuration
 public class RedisConfig {
+
+    private final ClientResources clientResources = DefaultClientResources.create();
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("redis-12924.c16.us-east-1-3.ec2.redns.redis-cloud.com");
+        config.setPort(12924);
+        config.setUsername("Baks");
+        config.setPassword("Toure224@@");
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientResources(clientResources)
+                .useSsl()
+                .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        clientResources.shutdown();
     }
 }
